c25e6e174452e4fab050b65e3c56660fb837b744
Gladys Ndichu <itsgladysbrian@gmail.com>
2022-06-09 13:41:42 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+let names = [];
+let celebration = "birthday";
+
+function writeCards(names, celebration){
+    return names.map(names=> `Thank you, ${names}, for the wonderful ${celebration} gift!` )
+}
+
+function countDown(num){
+    let i = num;
+    for (i=num; i>=0; i--){
+        console.log(i);
+    }
+}
+
